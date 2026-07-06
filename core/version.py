de2b9b32ef9de83d2e9f5fbe86d0f1f93fd81bb2
eugene <eugene.eighty20virtual@gmail.com>
2026-07-06 22:47:21 +0800

    update version

--- core/version.py
+++ core/version.py
@@ -3,5 +3,5 @@ version.py
 Contains the current application version and GitHub repository info for the auto-updater.
 """
 
-__version__ = "1.0.0"
+__version__ = "1.1.0"
 GITHUB_REPO = "eugeneknown/Speed-Test-Automation"
